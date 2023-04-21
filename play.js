783fa660ba35abcdc727f736d76001aab1bdb875
AliyaJK <kanani.aliya@gmail.com>
2023-04-21 23:06:40 +0000

    implement object destructuring

--- play.js
+++ play.js
@@ -1,20 +1,7 @@
-const net = require("net");
+const { connect } = require('./client')
 
 // establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: "localhost",
-      port: 50541,
-  });
 
-  conn.on("data", (data) => {
-    console.log(data);
-  });
-
-  conn.setEncoding("utf8");
-
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
